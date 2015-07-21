52ad7a56ccb8a83e415f688b5bfb8e5405f17528
Kiran <kunigirik@yahoo.com>
2015-07-21 10:36:41 -0700

    Initial Setup
    I have worked on this project before, but never put it up on Git. This
    is the initial setup in Git to start managing the project with Version
    Control.

---
+++ SwiftTimer/TimerController.swift
@@ -0,0 +1,184 @@
+//
+//  TimerController.swift
+//  SwiftTimer
+//
+//  Created by Kiran Kunigiri on 7/21/15.
+//  Copyright (c) 2015 Kiran Kunigiri. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TimerController: NSObject {
+    
+    //Properties
+    
+    // Will run every 0.1 seconds
+    var timer = NSTimer()
+    // The time when the timer started
+    var startTime = NSTimeInterval()
+    
+    // The UILabel we will have to update, along with an optional view if needed
+    var label = UILabel()
+    var view = UIView()
+    
+    // Properties of the label string
+    var strHours = "0"
+    var strMinutes = "00"
+    var strSeconds = "00"
+    
+    // The amount of time in the countdown
+    var totalTime: NSTimeInterval = 0
+    
+    // Properties of actual values
+    var hours = 0
+    var minutes = 0
+    var seconds = 0
+    
+    override init() {
+        
+    }
+    
+    // To create a timer with the label to update
+    init(viewLabel: UILabel) {
+        super.init()
+        
+        // Set the label and its text to default
+        label = viewLabel
+        updateLabel()
+    }
+    
+    // MARK: Main Update Function
+    func updateTime() {
+        var currentTime = NSDate.timeIntervalSinceReferenceDate()
+        
+        //Find the difference between current time and start time.
+        var elapsedTime: NSTimeInterval = currentTime - startTime
+        
+        //Set the elapsed time to the amount of time left in the timer
+        var timeLeft = totalTime - elapsedTime
+        
+        // Runs when the timer is over
+        if timeLeft <= 0 {
+            self.stop()
+        }
+        
+        //calculate the hours in elapsed time.
+        hours = Int(timeLeft / 3600.0)
+        timeLeft -= (NSTimeInterval(hours) * 3600)
+        
+        //calculate the minutes in elapsed time.
+        minutes = Int(timeLeft / 60.0)
+        timeLeft -= (NSTimeInterval(minutes) * 60)
+        
+        //calculate the seconds in elapsed time.
+        seconds = Int(timeLeft)
+        timeLeft -= NSTimeInterval(seconds)
+        
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        strHours = String(format: "%01d", hours)
+        strMinutes = String(format: "%02d", minutes)
+        strSeconds = String(format: "%02d", seconds)
+        
+        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
+        updateLabel()
+    }
+    
+    // MARK: Start/Stop Functions
+    func start() {
+        if !timer.valid {
+            let aSelector: Selector = "updateTime"
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
+            startTime = NSDate.timeIntervalSinceReferenceDate()
+        }
+    }
+    
+    func stop() {
+        timer.invalidate()
+        resetTimer()
+    }
+    
+    // MARK: Get Value Functions
+    
+    // Gets the time remaining in an NSTimeInterval
+    func getTimeLeft() -> NSTimeInterval {
+        var currentTime = NSDate.timeIntervalSinceReferenceDate()
+        //Find the difference between current time and start time.
+        var elapsedTime: NSTimeInterval = currentTime - startTime
+        //Set the elapsed time to the amount of time left in the timer
+        var timeLeft = totalTime - elapsedTime
+        return timeLeft
+    }
+    
+    // Gets the number of seconds completed in am NTimeInterval
+    func getTimeComplete() -> NSTimeInterval {
+        var timeLeft = getTimeLeft()
+        var timeComplete = totalTime - timeLeft
+        return timeComplete
+    }
+    
+    // Returns the percent of the timer completed with a decimal
+    func getPercentageCompleted() -> Double {
+        var timeLeft = getTimeLeft()
+        var timeComplete = totalTime - timeLeft
+        var percent = timeComplete/totalTime
+        
+        return percent
+    }
+    
+    // MARK: Set/Reset Functions
+    
+    // Same as the updateTimer, but is used to access outside this class
+    // Uses totalTime to update display instead of the time left in updateTime()
+    func resetDisplay() {
+        var timeLeft = totalTime
+        //calculate the hours in elapsed time.
+        hours = Int(timeLeft / 3600.0)
+        timeLeft -= (NSTimeInterval(hours) * 3600)
+        //calculate the minutes in elapsed time.
+        minutes = Int(timeLeft / 60.0)
+        timeLeft -= (NSTimeInterval(minutes) * 60)
+        //calculate the seconds in elapsed time.
+        seconds = Int(timeLeft)
+        timeLeft -= NSTimeInterval(seconds)
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        strHours = String(format: "%01d", hours)
+        strMinutes = String(format: "%02d", minutes)
+        strSeconds = String(format: "%02d", seconds)
+        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
+        updateLabel()
+    }
+    
+    func setTimeLeft(timeLeft: NSTimeInterval) {
+        totalTime = timeLeft
+        resetDisplay()
+    }
+    
+    func resetTimer() {
+        totalTime = NSTimeInterval(0)
+        resetDisplay()
+    }
+    
+    func resetTimerWithNewTime(newTotalTime: NSTimeInterval) {
+        self.totalTime = newTotalTime
+        startTime = NSDate.timeIntervalSinceReferenceDate()
+        var strHours = "0"
+        var strMinutes = "00"
+        var strSeconds = "00"
+        var hours = 0
+        var minutes = 0
+        var seconds = 0
+        updateLabel()
+    }
+    
+    // MARK: Private Update Functions
+    private func updateLabel() {
+        if hours > 0 {
+            label.text = "\(strHours):\(strMinutes):\(strSeconds)"
+        } else {
+            label.text = "\(strMinutes):\(strSeconds)"
+        }
+    }
+    
+}
+
--- SwiftTimer/ViewController.swift
+++ SwiftTimer/ViewController.swift
@@ -9,17 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    var mainTimer = TimerController()
+    var recognizer = UIPanGestureRecognizer()
+    var lastRecognizedInterval = CGPoint()
+    var kPanIntervalDistance : CGFloat = 30
+    var kPanIntervalSeconds = NSTimeInterval(300)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mainTimer = TimerController(viewLabel: timerLabel)
+        recognizer = UIPanGestureRecognizer(target: self, action: "screenSwiped:")
+        self.view.addGestureRecognizer(recognizer)
+        
+        mainTimer.setTimeLeft(NSTimeInterval(0))
+        mainTimer.view = self.view
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func start(sender: UIButton) {
+        mainTimer.start()
+    }
+    
+    @IBAction func stop(sender: UIButton) {
+        mainTimer.stop()
+    }
+    
+    // Allows user to swipe the screen to set the timer
+    @IBAction func screenSwiped(sender: UIPanGestureRecognizer) {
+        // Checks to make sure the timer is invalid and not yet running
+        if !mainTimer.timer.valid {
+            // Get total distance traveled in current pan
+            var thisInterval = recognizer.translationInView(self.view)
+            
+            // If the total distance minus the last distance point is greater than the minimum pan distance, pan the timer values
+            if lastRecognizedInterval.y - thisInterval.y > kPanIntervalDistance {
+                // Update the last distance point
+                lastRecognizedInterval = thisInterval;
+                // Update the timer value and display
+                mainTimer.totalTime += kPanIntervalSeconds
+                mainTimer.resetDisplay()
+            } else if -lastRecognizedInterval.y + thisInterval.y > kPanIntervalDistance {
+                // Update the last distance point
+                lastRecognizedInterval = thisInterval;
+                // Update the timer value and display, and set it to zero in case it becomes negative
+                mainTimer.totalTime -= kPanIntervalSeconds
+                if mainTimer.totalTime < 0 {
+                    mainTimer.totalTime = 0
+                }
+                mainTimer.resetDisplay()
+            }
+            // Reset the last distance point back to zero if all fingers lift
+            if sender.state == UIGestureRecognizerState.Ended {
+                lastRecognizedInterval = CGPoint()
+            }
+        }
+    }
+    
+    
+    
 }
 
